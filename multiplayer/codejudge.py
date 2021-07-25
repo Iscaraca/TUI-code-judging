2284158b8845e799dd5f57096ef915b0f814825b
Isaac Chen <49803282+Iscaraca@users.noreply.github.com>
2021-07-26 04:51:42 +0800

    Add multiplayer

---
+++ multiplayer/codejudge.py
@@ -0,0 +1,194 @@
+import curses
+from curses.textpad import Textbox, rectangle
+import timeit
+import socket
+import time
+
+# Connection with server
+s = socket.socket()
+s.settimeout(5)
+
+while True:
+    addr = input("Valid IP address of server (e.g. 127.0.0.1): ")
+    port = int(input("Port number (e.g. 22222): "))
+    try:
+        s.connect((addr, port))
+        break
+    except socket.error as exc:
+        print("Connection error: %s" % exc)
+
+
+def receive(s):
+    data = b""
+    instructionRequired = False
+    endRequired = False
+
+    while b"&&ENDL" not in data:
+        data += s.recv(1024)
+
+    if data.find(b"&&INSTRUCTION") != -1:
+        instructionRequired = True
+
+    if data.find(b"&&ENDG") != -1:
+        endRequired = True
+
+    # '&&ENDL' comes before other flags
+    data = data[: data.find(b"&&ENDL")]
+
+    return (data.decode(), instructionRequired, endRequired)
+
+
+def submit(s, timing, message):
+    message += "&&ENDL"
+    message += f"&&TIME={timing}"
+    s.sendall(message.encode())
+
+
+def draw_menu(stdscr):
+    stdscr.clear()
+    stdscr.refresh()
+
+    # Instructions box
+    helpOutlineULY = int(stdscr.getbegyx()[0])
+    helpOutlineULX = int(stdscr.getbegyx()[1])
+    helpOutlineLRY = int(stdscr.getbegyx()[0]) + 2
+    helpOutlineLRX = int(stdscr.getmaxyx()[1] - 1)
+
+    rectangle(stdscr, helpOutlineULY, helpOutlineULX, helpOutlineLRY, helpOutlineLRX)
+
+    # Editor box
+    writeboxOutlineULY = int(stdscr.getbegyx()[0]) + 3
+    writeboxOutlineULX = int(stdscr.getbegyx()[1])
+    writeboxOutlineLRY = int(stdscr.getmaxyx()[0] * 0.7) + 7
+    writeboxOutlineLRX = int(stdscr.getmaxyx()[1] - 1) - 49
+
+    rectangle(
+        stdscr,
+        writeboxOutlineULY,
+        writeboxOutlineULX,
+        writeboxOutlineLRY,
+        writeboxOutlineLRX,
+    )
+
+    stdscr.refresh()
+
+    writeboxH = int(stdscr.getmaxyx()[0] * 0.7) - int(stdscr.getbegyx()[0]) + 3
+    writeboxW = int(stdscr.getmaxyx()[1] - 1) - int(stdscr.getbegyx()[1]) - 51
+    writeboxURY = int(stdscr.getbegyx()[0]) + 4
+    writeboxURX = int(stdscr.getbegyx()[1]) + 1
+
+    writebox = curses.newwin(writeboxH, writeboxW, writeboxURY, writeboxURX)
+
+    # Results box
+    resultsOutlineULY = int(stdscr.getbegyx()[0]) + 3
+    resultsOutlineULX = int(stdscr.getmaxyx()[1] - 1) - 47
+    resultsOutlineLRY = int(stdscr.getmaxyx()[0] * 0.7) + 7
+    resultsOutlineLRX = int(stdscr.getmaxyx()[1] - 1)
+
+    rectangle(
+        stdscr,
+        resultsOutlineULY,
+        resultsOutlineULX,
+        resultsOutlineLRY,
+        resultsOutlineLRX,
+    )
+
+    resultsH = int(stdscr.getmaxyx()[0] * 0.7) - int(stdscr.getbegyx()[0]) + 2
+    resultsW = int(stdscr.getmaxyx()[1] - 1) - int(stdscr.getmaxyx()[1] - 1) + 45
+    resultsURY = int(stdscr.getbegyx()[0]) + 4
+    resultsURX = int(stdscr.getmaxyx()[1] - 1) - 46
+
+    results = curses.newwin(resultsH, resultsW, resultsURY, resultsURX)
+
+    stdscr.refresh()
+
+    helpH = 1
+    helpW = int(stdscr.getmaxyx()[1] - 2)
+    helpURY = 1
+    helpURX = 1
+
+    helpbox = curses.newwin(helpH, helpW, helpURY, helpURX)
+
+    helpbox.addstr(0, 0, f"{receive(s)[0]}. The game will start shortly.")
+    helpbox.refresh()
+    time.sleep(3)
+
+    while True:
+        response, instructionRequired, endRequired = receive(s)
+        if instructionRequired:  # new question.
+            helpbox.clear()
+            helpbox.addstr(0, 0, f"{response}(CTRL-G to submit)")
+            helpbox.refresh()
+
+            results.clear()
+            results.refresh()
+
+            startCode = timeit.default_timer()
+
+            # Allow for text input
+            writebox.clear()
+            writebox.refresh()
+            box = Textbox(writebox)
+            box.edit()
+
+            message = box.gather()
+
+            endCode = timeit.default_timer()
+
+            submit(s, ((endCode - startCode) * 1000.0), message)
+
+            helpbox.clear()
+            helpbox.addstr(0, 0, "Code has been submitted. Please wait for a while.")
+            helpbox.refresh()
+
+        elif endRequired:  # end game
+            results.addstr(0, 0, response)
+            results.refresh()
+            k = stdscr.getch()
+            break
+
+        else:  # display results for that question
+            results.addstr(0, 0, f"{response}\n\n(Press any key to continue)")
+            results.refresh()
+            k = stdscr.getch()
+
+
+def main():
+    curses.wrapper(draw_menu)
+
+
+if __name__ == "__main__":
+    main()
+
+    # # Test code
+    # # Test cases, [input, output]
+    # cases = [
+    #     [[5, 4, 3, 2, 1, 0], [0, 1, 2, 3, 4, 5]],
+    #     [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]],
+    #     [[1, 6, 6, 5, 3, 4], [1, 3, 4, 5, 6, 6]]]
+
+    # sorted_l = []
+
+    # times = []
+
+    # correctCases = 0
+
+    # for case in cases:
+    #     messageNew = f"sorted_l = []\nl = {str(case[0])}\n" + message
+
+    #     startExec = timeit.default_timer()
+
+    #     loc = {}
+    #     exec(messageNew, globals(), loc)
+    #     sorted_l = loc['sorted_l']
+
+    #     endExec = timeit.default_timer()
+
+    #     times.append((endExec - startExec) * 1000.0)
+    #     if sorted_l == case[1]:
+    #         correctCases += 1
+
+    # caseResults = "Execution times\n" + '\n'.join(["Case {}: {:.5f}ms".format(i, times[i]) for i in range(len(times))])
+    # accuracy = correctCases / len(cases)
+
+    # results.addstr(0, 0, caseResults + "\n\nAccuracy: {:.2f}%".format(accuracy * 100.0) + "\n\nTime taken to type: {:.2f}s".format(endCode - startCode) + "\n\n\n\n\nPress any key to exit")
---
+++ multiplayer/server.py
@@ -0,0 +1,188 @@
+import socket
+from multiprocessing import *
+import time
+import json
+import sys
+
+players = []
+
+
+def send(player, string, endRequired=False, instructionRequired=False):
+    """Appends appropriate event identifier string to transmission
+
+    Args:
+        player (tuple<socket, address>): Target of transmission
+        string (string): Transmission data
+        endRequired (bool, optional): Triggers end of game event. Defaults to False.
+        instructionRequired (bool, optional): Routes transmission data to the instructions window. Defaults to False.
+    """
+    string += "&&ENDL"
+
+    if instructionRequired:
+        string += "&&INSTRUCTION"
+
+    if endRequired:
+        string += "&&ENDG"
+
+    # player is a tuple (socket, address)
+    print(f"sent {string} to player {player}")
+    player[0].sendall(string.encode())
+
+
+def sendAll(string, endRequired=False, instructionRequired=False):
+    """Broadcasts transmission to all players
+
+    Args:
+        string (string): Transmission data
+        endRequired (bool, optional): Triggers end of game event. Defaults to False.
+        instructionRequired (bool, optional): Routes transmission data to the instructions window. Defaults to False.
+    """
+    for player in players:
+        send(player, string, endRequired, instructionRequired)
+        time.sleep(0.1)  # A result of blocking sockets, so we don't have to deal with any race conditions
+
+
+def acceptPlayers(s):
+    """Accepts connection requests from two clients
+
+    Args:
+        s (socket object): The socket object
+    """
+    while len(players) < 2:
+        sock, addr = s.accept()
+        players.append([sock, addr, 0, 0.0])  # socket, address, score, timing
+        send([sock], f"You are connected. You are player number {len(players)}")
+
+
+def parseConfig(filename):
+    """Parses data from config file, namely:
+        	line (integer): Unique challenge identifier
+			skip (bool): Indicates whether a question will appear in a challenge
+			prepend (string): Code block to run before input executes
+			append (string): Code block to run after input executes
+			instructions (string): Prompt that displays on instruction window
+
+        and parses data from the answer file
+
+    Args:
+        filename (string): Path to config file
+
+    Returns:
+        tuple<dict, list>: Loaded config file and list of recommended answers
+    """
+    config_file = open(str(filename), "r")
+    config = json.loads(config_file.read())
+    code_file = open(str(config["file"]), "r")
+    code = code_file.read().splitlines()
+    return config["config"], code
+
+
+def findCase(config, line):
+    return [case for case in config if case["line"] == int(line)][0]
+
+
+def check(case, response):
+    if case["skip"]:
+        return True
+
+    try:
+        loc = {}
+        exec(f"{case['prepend']}{response}{case['append']}", globals(), loc)
+        return loc["test"]
+    except:
+        print("something went wrong with execution of check. ")
+        return False
+
+
+def receive():
+    """Receives time and code from clients
+
+    Returns:
+        list: 2D array with each element representing time and code data
+    """
+    responses = []
+
+    for index, player in enumerate(players):
+        response = b""
+        while b"&&ENDL" not in response:
+            try:
+                response += player[0].recv(1024)
+            except:
+                print("Receiving failed.")
+                sys.exit(1)
+
+        timing = response[response.find(b"&&TIME=") + 7 :].decode()
+        response = response[: response.find(b"&&ENDL")].decode()
+
+        responses.append([index, timing, response])
+
+    return responses
+
+
+def battle(filename):
+    """Main function for the server, executes received code in the context of a larger code block
+    and checks for sanity in execution
+
+    Args:
+        filename (string): Path to config file
+
+    Returns:
+        bool: Execution status
+    """
+    s = socket.socket()
+    addr = input("Valid IP address of server (e.g. 127.0.0.1): ")
+    port = int(input("Port number (e.g. 22222): "))
+    try:
+        s.bind((addr, port))
+        print("Bind successful")
+        s.listen()
+        print("Listening...")
+    except socket.error as exc:
+            s.close()
+            s = None
+    if s is None:
+        print('could not open socket')
+        sys.exit(1)
+
+    acceptPlayers(s)
+
+    if len(players) < 2:
+        return False
+
+    config, codes = parseConfig(filename)
+
+    for line, code in enumerate(codes):
+        case = findCase(config, line)
+
+        sendAll(case["instructions"], instructionRequired=True)
+
+        responses = receive()
+        print(responses)
+        for [index, timing, response] in responses:
+            print(response)
+            result = check(case, response)
+            players[index][3] += float(timing)
+            if result:
+                players[index][2] += 1
+            send(
+                players[index],
+                f"Your code is {('correct' if result else 'wrong')}.\n\nYou took {timing/1000:.2f}s\n\nFor your reference, the model answer is:\n\t{code}"
+            )
+
+    sendAll(
+        f"Here are the scores:\n\nPlayer 1:\n\tScore:{players[0][2]}\n\tTiming:{players[0][3]/1000:.2f}s\n\nPlayer 2:\n\tScore:{players[1][2]}\n\tTiming:{players[1][3]/1000:.2f}s",
+        endRequired=True,
+    )
+
+    # clean up
+    for player in players:
+        player[0].close()
+    s.close()
+
+    return True
+
+
+if __name__ == "__main__":
+    print("Server Online!")
+    battle("./config.json")
+    print("Server Terminated.")
